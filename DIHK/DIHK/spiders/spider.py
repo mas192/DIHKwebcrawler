8a85ec0d18fde3be50928ca9bba843785f0afc7a
mas192 <samjoshva@rediffmail.com>
2020-07-23 14:41:32 -0700

    gettig name and title

---
+++ DIHK/DIHK/spiders/spider.py
@@ -0,0 +1,46 @@
+import scrapy
+
+
+class SpiderSpider(scrapy.Spider):
+    name = 'spider'
+    allowed_domains = ['dihk.de']
+    start_urls = ['https://www.dihk.de/de/ueber-uns/die-ihk-organisation/dihk-deinternational-gmbh-12906/']
+
+    def parse(self, response):
+
+        # contacts = response.css('div.teaser-contact__content').getall()
+        # print(contacts[0])
+        # print(contacts[1])
+
+
+        for contact in response.css('div.teaser-contact__content'):
+            name = response.css('span.teaser-contact__contact-name::text').getall()
+            title = response.css('span.teaser-contact__contact-title::text').getall()
+            ntDict = dict(Name=name, Title=title)
+
+
+
+# <div class="teaser-contact__contact">
+# <span class="teaser-contact__contact-name">Benjamin Leipold</span>
+# <span class="teaser-contact__contact-title">Geschäftsführer DIHK DEinternational GmbH</span>
+# </div>
+#
+#
+# <div class="teaser-contact__content">
+# <div class="teaser-contact__contact">
+# <span class="teaser-contact__contact-name">Benjamin Leipold</span>
+# <span class="teaser-contact__contact-title">Geschäftsführer DIHK DEinternational GmbH</span>
+# </div>
+# <div class="teaser-contact__info">
+# <a href="tel:+4930203082400" class="teaser-contact__info-telephone">+49 30 20308 2400</a>
+# <a href="mailto:leipold.benjamin@dihk.de" class="teaser-contact__info-email">leipold.benjamin@dihk.de</a>
+# </div>
+# </div>
+#
+#
+# <div class="teaser-contact__info">
+# <a href="tel:+4930203082400" class="teaser-contact__info-telephone">+49 30 20308 2400</a>
+# <a href="mailto:leipold.benjamin@dihk.de" class="teaser-contact__info-email">leipold.benjamin@dihk.de</a>
+# </div>
+# </div>
+# </div>
